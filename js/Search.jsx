5ae78b2c0b00d4ba44e5c693551a390634a55f54
Steven Bassett <steven.j.bassett@gmail.com>
2016-03-24 13:50:19 -0700

    Update: add fileters

--- js/Search.jsx
+++ js/Search.jsx
@@ -1,19 +1,44 @@
-import React from 'react'
+import React, { Component } from 'react'
 import data from '../public/data'
 import ShowCard from './ShowCard'
 
-const Search = () => (
-  <div className='container'>
-    <header className='header'>
-      <h1 className='brand'>sVideo</h1>
-      <input type='search' placeholder='search' className='search-input' />
-    </header>
-    <div className='shows'>
-      {data.shows.map((show, index) => (
-        <ShowCard {...show} key={show.imdbID} />
-      ))}
-    </div>
-  </div>
-)
+export default class Search extends Component {
+  constructor (...args) {
+    super(...args)
+    this.state = {
+      searchTerm: ''
+    }
+
+    this.handleSearchTermEvent = this.handleSearchTermEvent.bind(this)
+  }
+
+  handleSearchTermEvent (e) {
+    this.setState({ searchTerm: e.target.value })
+  }
+
+  render () {
+    const searchShows = (show) => {
+      if (this.state.searchTerm.length) {
+        return `${show.title} ${show.description}`.toUpperCase().indexOf(this.state.searchTerm.toUpperCase()) >= 0
+      } else {
+        return true
+      }
+    }
+
+    return (
+      <div className='container'>
+        <header className='header'>
+          <h1 className='brand'>sVideo</h1>
+          <input type='search' placeholder='search' className='search-input' value={this.state.searchTerm} onChange={this.handleSearchTermEvent} />
+        </header>
+        <div className='shows'>
+          {data.shows.filter(searchShows).map((show, index) => (
+            <ShowCard {...show} key={show.imdbID} />
+          ))}
+        </div>
+      </div>
+    )
+  }
+}
 
 export default Search
