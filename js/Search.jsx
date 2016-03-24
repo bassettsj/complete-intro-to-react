81b6aad689d5954122357a12c19bc941547437a8
Steven Bassett <steven.j.bassett@gmail.com>
2016-03-24 13:23:15 -0700

    Update:add search

--- js/Search.jsx
+++ js/Search.jsx
@@ -4,6 +4,10 @@ import ShowCard from './ShowCard'
 
 const Search = () => (
   <div className='container'>
+    <header className='header'>
+      <h1 className='brand'>sVideo</h1>
+      <input type='search' placeholder='search' className='search-input' />
+    </header>
     <div className='shows'>
       {data.shows.map((show, index) => (
         <ShowCard {...show} key={show.imdbID} />
